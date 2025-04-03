f4ce43d32c2c89dd2699bf0fb42c50da4c7b2082
unknown <160930990+bergis952@users.noreply.github.com>
2025-04-03 12:47:54 -0500

    Anime opening to rival Evangelion

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer vp;
+    void Start()
+    {
+        vp = gameObject.GetComponent<VideoPlayer>();
+        vp.Play();
+        StartCoroutine (CheckVideoComplete());
+    }
+    IEnumerator CheckVideoComplete()
+    {
+        // Wait until the video starts playing
+        while (!vp.isPlaying)
+        {
+            yield return null;
+        }
+        //now it is playing
+        while (vp.isPlaying)
+        {
+            yield return null;
+        }
+        //bnow it is done
+        SceneManager.LoadScene("StartVillage");
+    }
+}
